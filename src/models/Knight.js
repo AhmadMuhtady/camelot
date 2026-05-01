3f98cdd9303c69bbd88b2f7c4cd9137220d59fd2
AhmadMuhtady <ahmad.d.muhtadi@gmail.com>
2026-05-02 01:50:34 +0300

    added init files

---
+++ src/models/Knight.js
@@ -0,0 +1,12 @@
+export class Knight {
+	constructor(config) {
+		this.id = config.id;
+		this.name = config.name;
+		this.emoji = config.emoji;
+		this.model = config.model;
+		this.color = config.color;
+		glow;
+		active;
+		tokens;
+	}
+}
